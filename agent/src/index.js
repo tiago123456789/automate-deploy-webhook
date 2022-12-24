0aa8b5df41bfeb40089e318c9801f53a0efbf5fb
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-12-24 13:17:07 -0300

    Added validation

--- agent/src/index.js
+++ agent/src/index.js
@@ -3,8 +3,13 @@ const { execSync } = require("child_process")
 const app = express();
 
 app.get("/update-app", async (req, res) => {
+    const { query } = req.query;
+    if (query?.key !== '9874697e-8132-4d1b-a611-29885f3fac8e') {
+        return res.status(403).json({ status: 403, error: "You can't execute this action" })
+    }
+
     execSync('cd ../api && npm install && git status ', { stdio: [0,1,2] });
-    return res.json({ message: "Updated application success." })
+    return res.json({ status: 200, message: "Updated application success." })
 })
 
 
