55add29001b7f9892ad8e405ed281493c9cffc56
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-12-24 13:52:22 -0300

    Changes

--- agent/src/index.js
+++ agent/src/index.js
@@ -5,12 +5,12 @@ const app = express();
 const key = '9874697e-8132-4d1b-a611-29885f3fac8e'
 
 app.get("/update-app", async (req, res) => {
-    const { query } = req.query;
-    if (query?.key !== key) {
+    const { query } = req;
+    if (query?.key != key) {
         return res.status(403).json({ status: 403, error: "You can't execute this action" })
     }
 
-    execSync('cd ../api && npm install && git status ', { stdio: [0,1,2] });
+    execSync(`cd ../api && git fetch && git checkout master && git pull origin master && npm install && pm2 restart api`, { stdio: [0,1,2] });
     return res.json({ status: 200, message: "Updated application success." })
 })
 
