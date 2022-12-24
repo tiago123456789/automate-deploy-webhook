47f1e647c0202f54d04bce4212e39b8007967c01
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-12-24 14:19:17 -0300

    Changes to test update deploy using webhook

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.json({ message: "Hello world 222!" })
+    return res.json({ message: "Hello world! Olá mundo" })
 })
 
 
