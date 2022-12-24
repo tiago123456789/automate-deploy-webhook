9af4a9a2649101c52b72ed7737890fb70c75937d
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-12-24 14:26:47 -0300

    Changes

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.json({ message: "Olá mundo atualizar novamente" })
+    return res.json({ message: "Olá mundo atualizar novamente 2" })
 })
 
 
